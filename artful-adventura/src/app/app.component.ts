393ad6cecdd7646b677795b7135c1067ffd67750
Atif Ahmad <Atif Ahmad>
2024-08-22 12:43:03 +0500

    Fixed footer icons and about us page content

--- artful-adventura/src/app/app.component.ts
+++ artful-adventura/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { LandingComponent } from "./components/landing/landing.component";
-import { HeaderComponent } from "./components/landing/header/header.component";
-import { FooterComponent } from "./components/landing/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
 	selector: "app-root",
